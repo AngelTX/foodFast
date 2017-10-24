2f6de953a2608ed35f197f5b16be6952fc82b90f
Miguel Bernal <miguel.bernal@g.austincc.edu>
2017-10-23 19:15:06 -0500

    reverted changes again

--- src/components/menu.js
+++ src/components/menu.js
@@ -11,31 +11,15 @@ class Menu extends Component{
   };
 
   componentWillMount() {
-    fetch('https://api.nutritionix.com/v1_1/search', {
+    fetch('http://localhost:3001/menu', {
       method: 'POST',
-      headers:{
+      headers : {
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "appId":"f6c1ec63",
-        "appKey":"75d1892186aa45da19a882eb81ba37ba",
-        "fields":["item_name","brand_name","nf_calories","images_front_full_url", "upc"],
-        "sort":{
-          "field":"_score",
-          "order":"desc"
-        },
-        "query": "starbucks",
-        "offset":0,
-        "limit":3,
-        "filters":{
-          "item_type":1,
-          "nf_calories":{
-            "from":500,
-            "to":700
-          }
-        }
-      })
-    }).then(results => {
+        'Accept': 'application/json',
+        mode: 'no-cors',
+      }
+    })
+    .then(results => {
       return results.json();
     })
     .then(data => {
@@ -50,7 +34,6 @@ class Menu extends Component{
         });
     })
   }
-
 
   render(){
     return(
