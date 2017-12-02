69f9948612489f45eb98c4235abcd1e9dfc42320
Miguel Bernal <miguel.bernal@g.austincc.edu>
2017-12-02 11:23:25 -0600

    cleaned up some code. Removed experimental stuff to look cleaner.

--- src/containers/MenuContainer.js
+++ src/containers/MenuContainer.js
@@ -2,36 +2,35 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 
 import MealCategory from './MealCategory.js';
-// import LunchContainer from './Lunch.js';
-// import DinnerContainer from './Dinner.js';
 const MenuContainer = (props) =>{
 
     return(
-      <div><div>
-        <Row className="menu">
-          <Col xs={4}>
-            <Col xs={12}>
-              <div>
-                <MealCategory items={props.menuItems} category={"Breakfast"}/>
-              </div>
-            </Col><br/>
-          </Col>
-          <Col xs={4}>
-            <Col xs={12}>
-              <div>
-                <MealCategory items={props.menuItems} category={"Lunch"}/>
-              </div>
-            </Col><br/>
-          </Col>
-          <Col xs={4}>
-            <Col xs={12}>
-              <div>
-                <MealCategory items={props.menuItems} category={"Dinner"}/>
-              </div>
-            </Col><br/>
-          </Col>
-        </Row>
-      </div>
+      <div>
+        <div>
+          <Row className="menu">
+            <Col xs={4}>
+              <Col xs={12}>
+                <div>
+                  <MealCategory items={props.menuItems} category={"Breakfast"}/>
+                </div>
+              </Col><br/>
+            </Col>
+            <Col xs={4}>
+              <Col xs={12}>
+                <div>
+                  <MealCategory items={props.menuItems} category={"Lunch"}/>
+                </div>
+              </Col><br/>
+            </Col>
+            <Col xs={4}>
+              <Col xs={12}>
+                <div>
+                  <MealCategory items={props.menuItems} category={"Dinner"}/>
+                </div>
+              </Col><br/>
+            </Col>
+          </Row>
+        </div>
       </div>
     )
 }
